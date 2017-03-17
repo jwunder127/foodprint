64c589cd2ab75dede877344e1314a51177906800
John Underwood <john.w.underwood.iii@gmail.com>
2017-03-17 18:06:33 -0400

    feat(Provider): add provider

--- foodprintReactNative/index.android.js
+++ foodprintReactNative/index.android.js
@@ -6,6 +6,7 @@
 
 import React, { Component } from 'react';
 import { Router, Scene } from 'react-native-router-flux';
+import { Provider } from 'react-redux';
 import Home from './android/components/Home'
 import Login from './android/components/Login'
 import Calendar from './android/components/Calendar'
@@ -32,6 +33,7 @@ export default class foodprintReactNative extends Component {
   render() {
     welcome();
     return (
+    <Provider store={store}>
       <Router>
         <Scene key="root">
           <Scene key="login" component={Login} title="Login" initial = {true}/>
@@ -41,6 +43,7 @@ export default class foodprintReactNative extends Component {
           <Scene key="meal" component={Meal} title= "Meal View"/>
        </Scene>
       </Router>
+    </Provider>
     )
   }
 }
