094f13711c111ec85dc2cd9487e3dc386b177624
Zohaib Rahman <zohaibr@gmail.com>
2017-03-24 18:01:34 -0400

    Getting SignUp from other branch

--- foodprintReactNative/reducers/auth.js
+++ foodprintReactNative/reducers/auth.js
@@ -20,25 +20,25 @@ export const authenticated = user => ({
 
 export const whoami = () =>
   dispatch =>
-    axios.get('http://192.168.5.50:1337/api/auth/whoami')
+    axios.get('http://192.168.5.51:1337/api/auth/whoami')
       .then(response => {
         const user = response.data
         dispatch(authenticated(user))
       })
-      .catch((err) => {
+      .catch(() => {
         dispatch(authenticated(null))
       })
 
 export const login = (username, password) =>
   dispatch =>
-    axios.post('http://192.168.5.50:1337/api/auth/login/local',
+    axios.post('http://192.168.5.51:1337/api/auth/login/local',
       {username, password})
       .then(() => dispatch(whoami()))
       .catch(() => dispatch(whoami()))
 
 export const logout = () =>
   dispatch =>
-    axios.post('http://192.168.5.50:1337/api/auth/logout')
+    axios.post('http://192.168.5.51:1337/api/auth/logout')
       .then(() => dispatch(whoami()))
       .catch(() => dispatch(whoami()))
 
